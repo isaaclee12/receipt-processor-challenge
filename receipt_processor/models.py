17389a99b650fdcbae0d637f8f6877d509aabbcb
isaaclee12 <isaac.wonha.lee@outlook.com>
2024-11-29 23:00:11 -0500

    feat: completed ReceiptView, returns uuid
    - also modified model to include uuid + better many to many relationship

--- receipt_processor/models.py
+++ receipt_processor/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 from datetime import datetime
+import uuid
 
 class Item(models.Model):
-    short_description = models.CharField(max_length=100, blank=False, null=False, default="")
+    short_description = models.CharField(max_length=100, blank=False, null=False, default='')
     price = models.DecimalField(max_digits=20, decimal_places=2, blank=False, null=False, default=0)
 
 class Receipt(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     retailer = models.CharField(max_length=30, blank=False, null=False)
     purchase_datetime = models.DateTimeField(blank=False, null=False)
     total = models.DecimalField(max_digits=20, decimal_places=2, blank=False, null=False)
     # create a join table with foreign keys to Receipt and Item
-    items = models.ManyToManyField(Item, through="ItemsOnReceipt")
+    items = models.ManyToManyField(Item, through='ItemAssignmentToReceipt')
 
-class ItemsOnReceipt(models.Model):
+class ItemAssignmentToReceipt(models.Model):
     receipt = models.ForeignKey(Receipt, on_delete=models.CASCADE)
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
--- receipt_processor/views.py
+++ receipt_processor/views.py
@@ -2,10 +2,11 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from receipt_processor.models import Receipt
+from receipt_processor.models import Item, ItemAssignmentToReceipt, Receipt
 
 from datetime import datetime
 import json
+
 
 class ReceiptView(APIView):
     """
@@ -16,7 +17,7 @@ class ReceiptView(APIView):
             Creates a new Receipt
     """
 
-    permission_classes = (AllowAny, )
+    permission_classes = (AllowAny,)
     http_method_names = ['post', 'head']
 
     def post(self, request):
@@ -28,29 +29,38 @@ class ReceiptView(APIView):
         items = request_body.get('items')
 
         # Data cleaning
-        year, month, day = [int(x) for x in purchase_date.split("-")]
-        hour, minute = [int(x) for x in purchase_time.split(":")]
+        year, month, day = [int(x) for x in purchase_date.split('-')]
+        hour, minute = [int(x) for x in purchase_time.split(':')]
         purchase_datetime = datetime(year=year, month=month, day=day, hour=hour, minute=minute)
         total = float(total)
-
-        print("\n\n\nITEMS", items)
-        # for item in items:
 
         # Create a Receipt, (or get if it already exists), extract the id.
         receipt, _ = Receipt.objects.get_or_create(
             retailer=retailer,
             purchase_datetime=purchase_datetime,
             total=total,
-            items=items,
+            # items=items,
         )
 
-        print("RECEIPT", receipt)
+        # Get or create the items
+        for item in items:
+            item_object, _ = Item.objects.get_or_create(
+                short_description=item.get('shortDescription'),
+                price=item.get('price'),
+                )
 
-        # Return id
-        return Response(data={"id": receipt.id}, status=200)
+            ItemAssignmentToReceipt.objects.get_or_create(
+                item=item_object,
+                receipt=receipt,
+            )
+
+            receipt.items.all()
+
+        return Response(data={'id': receipt.id}, status=200)
 
 
 class ReceiptPointsView(APIView):
+
     def get(self):
         # If a receipt exists for the ID that has the point value, return the points.
         # Else, generate the point value and return it.
